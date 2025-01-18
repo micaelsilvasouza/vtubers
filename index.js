97901a73597f0ef0102c577960c69baf3f41aceb
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-01-18 18:39:28 -0300

    Estilos da página de exibição do vtuber

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var vtubers = {
     "toshiruz": {
+        "id": "toshiruz",
         "name": "Toshiruz",
         "description": "Um ser, meio humano e meio oni, com poderes misticos, que viajou ao passado para reunir pessoas com poderes especiais, para evitar que algo ruim aconteça, formando um grupo chamado GUREN.",
         "links": {
-            "youtube": ["https://www.youtube.com/@Toshiruz", "https://www.youtube.com/@ToshiruzLives"],
+            "youtube": "https://www.youtube.com/@ToshiruzLives",
             "twitch": "https://www.twitch.tv/toshiruz",
             "x": "https://x.com/toshiruz",
-            "instagram": " https://www.instagram.com/toshiruz/",
+            "instagram": "https://www.instagram.com/toshiruz/",
             "tiktok": "https://www.tiktok.com/@toshiruz"
         },
         "video": "https://www.youtube.com/embed/e0YnTzrxDDs?si=UUXfaAD5hbQes_fI"
     },
 
     "dantewill": {
+        "id": "dantewill",
         "name": "DanteWill",
         "description": "Um ser que possui duas formas, morcego e aranha, o protetor da grande árvore Yggdrasil, guardião da chave que abre todas as portas que se conectão à outros universos.",
         "links":{
             "youtube": "https://www.youtube.com/@DanteWill",
+            "twitch": "https://www.twitch.tv/dante_will",
             "x": "https://x.com/thedantewill",
             "instagram": "https://www.instagram.com/thedantewill",
             "tiktok": "https://www.tiktok.com/@thedantewill"
@@ -25,6 +28,7 @@ var vtubers = {
     },
 
     "eeiris": {
+        "id": "eeiris",
         "name": "Eeiris",
         "description": "Um Kitsune interdimensional que se manifestou nesse mundo, tendo apenas memórias vagas de outras vidas/dimensões e descobrindo meus poderes, tentando descobrir e curmpri missão aqui na Terra.",
         "links":{
@@ -32,14 +36,13 @@ var vtubers = {
             "twitch":"https://www.twitch.tv/eeiris",
             "x": "https://x.com/Eeirisz",
             "instagram": "https://www.instagram.com/eeirisz/",
-            "tiktok": "https://www.tiktok.com/@eeirisz",
-            "discord": "https://discord.com/invite/SmZADC4syC",
-            "credits": "https://eeiris.carrd.co/"
+            "tiktok": "https://www.tiktok.com/@eeirisz"
         }, 
         "video": "https://www.youtube.com/embed/CZrNAYM0frw?si=2pYCnrVDl--yX_-j"
     }, 
 
-    "mii-reis": {
+    "mii_reis": {
+        "id": "mii_reis",
         "name": "Mii Reis",
         "description": "Uma híbrida de humano com partes felinas, material de uma pesquisa de inteligencias artificiais, que ser tornou uma SEO, e agora está em busca de talentos excepcionais.",
         "links": {
@@ -52,7 +55,8 @@ var vtubers = {
         "video": "https://www.youtube.com/embed/Dho3MsiQuI0?si=ijxURzsZyeafXCtN"
     }, 
 
-    "raviel-karin": {
+    "raviel_karin": {
+        "id": "raviel_karin",
         "name": "Raviel Karin",
         "description": "Um explorador que entrou nas ruinas do tumulo de um faraó, encontrando um colar e morrendo por uma armadilha, fazendo um acordo com Anúbis, voltando a vida em troca de nunca tirar o colar.",
         "links": {
@@ -62,10 +66,11 @@ var vtubers = {
             "instagram": "https://www.instagram.com/raviel_karim/",
             "tiktok": "https://www.tiktok.com/@ravielkarimz"
         },
-        "video": "https://www.youtube.com/embed/Kdc5aBupFcY?si=grXbTgFu5O1HMAPu"
+        "video": "https://www.youtube.com/embed/6qA1VeCdgUw?si=L1gNn88GI1MemW3l"
     },
 
-    "eveline-teach":{
+    "eveline_teach":{
+        "id": "eveline_teach",
         "name": "Eveline Teach",
         "description": "Uma ex-deliquente que ao invadiu um museu para pegar um colar, descobriu era decendentes de piratas, e assim decidiu seguiria os passos de seus ancestrais e ser uma pirata.",
         "links":{
@@ -75,12 +80,13 @@ var vtubers = {
             "instagram": "https://www.instagram.com/evelineteach/",
             "tiktok": "https://www.tiktok.com/@evelineteach"
         },
-        "video": "https://www.youtube.com/embed/RLKzixdLGGI?si=nKkezrniB5G6wtYn"
+        "video": "https://www.youtube.com/embed/axP4FtWUBi4?si=ntzX_X6JCISCnJiv"
     },
 
-    "mono-momo": {
-        "name": "MONO & MOMO",
-        "description": "Seguindo um colar que as protegia, as irmãs encotraram uma empresa suspeita, decidindo invadir o lugar, junto de sua mentora, mas sua mentora desapareceu, deixando elas com mais um problema para resolver",
+    "mono": {
+        "id": "mono",
+        "name": "Mono",
+        "description": "Irmã de Momo que, seguindo um colar que as protegia, encotraram uma empresa suspeita, decidindo invadir o lugar, com sua mentora, mas ela desapareceu, deixando elas com mais um problema para resolver",
         "links": {
             "youtube": "https://www.youtube.com/@MonoMomoChan",
             "twitch": "https://www.twitch.tv/monomomochan",
@@ -91,7 +97,22 @@ var vtubers = {
         "video": "https://www.youtube.com/embed/qM0O-s7p_Dw?si=L7CFO7jpE9GKGBK0"
     },
 
-    "emi-kotone": {
+    "momo": {
+        "id": "momo",
+        "name": "Momo",
+        "description": "Irmã de Mono que, seguindo um colar que as protegia, encotraram uma empresa suspeita, decidindo invadir o lugar, com sua mentora, mas ela desapareceu, deixando elas com mais um problema para resolver",
+        "links": {
+            "youtube": "https://www.youtube.com/@MonoMomoChan",
+            "twitch": "https://www.twitch.tv/monomomochan",
+            "x": "https://x.com/monomomochan",
+            "instagram": "https://www.instagram.com/monomomochan/",
+            "tiktok": "https://www.tiktok.com/@monomomochan" 
+        }, 
+        "video": "https://www.youtube.com/embed/qM0O-s7p_Dw?si=L7CFO7jpE9GKGBK0"
+    },
+
+    "emi_kotone": {
+        "id": "emi_kotone",
         "name": "Emi Kotone",
         "description": "Guardiã do Frasco da Virtude, manisfestada na forma de uma gata-humana, responsável por evitar que o frasco no coração de cada pessoa fique vazio, para que não fiquem cheias de dor e desespero.",
         "links": {
@@ -105,31 +126,33 @@ var vtubers = {
     },
 
     "torajo": {
+        "id": "torajo",
         "name": "Torajo Jovem",
         "description": "Vindo de um planeta chamado Verade, no qual todos os habitantes são frutas humanoides, é dono de um canal grande onde com a ajuda do 'Algorithomus', a energia do planeta, seus videos são compartilhados.",
         "links": {
-            "youtube": ["https://www.youtube.com/@TorajoJovem", "https://www.youtube.com/@Torajo"],
+            "youtube": "https://www.youtube.com/@Torajo",
             "x": "https://x.com/Torajo_yt",
             "instagram": "https://www.instagram.com/mundo.torajo/",
             "tiktok": "https://www.tiktok.com/@torajo_yt"
         },
-        "video": "https://www.youtube.com/embed/w4oUvNRYz1E?si=qFXLizw--0ZolvYZ"
+        "video": "https://www.youtube.com/embed/mvar3kgPKyU?si=W_DP5IFDqKxHuYFZ"
     },
 
-    "jake-ito":{
+    "jake_ito":{
+        "id": "jake_ito",
         "name": "Jake Ito",
         "description": "Um inventor de uma Londres alternativa Steampunk, almeja salvar seu mundo das péssimas condições ambientais, através de uma oscilação no espaço-tempo pode realizar a conexão que ele tanto buscou.",
         "links": {
             "youtube": "https://www.youtube.com/@JakeIto_",
             "x": "https://x.com/JakeIto_",
             "instagram": "https://www.instagram.com/jake.ito_/",
-            "tiktok": "https://www.tiktok.com/@jakeito_",
-            "threads": "https://www.threads.net/@jake.ito_"
+            "tiktok": "https://www.tiktok.com/@jakeito_"
         },
-        "video": "https://www.youtube.com/embed/R2NSQ81kHfc?si=axdISQjP3sez9bWo"
+        "video": "https://www.youtube.com/embed/0OmVK-l9-Yw?si=ub4BbAPpmlVXfuRw"
     },
 
-    "jesse-llun": {
+    "jesse_llun": {
+        "id": "jesse_llun",
         "name": "Jesse Llun",
         "description": "Uma estrela mágica que ganhou vida de um desejo, e veio pra terra transformar os desejos das pessoas em realidade... ou quase isso!",
         "links":{
@@ -138,7 +161,7 @@ var vtubers = {
             "instagram": "https://www.instagram.com/jessellum/",
             "tiktok": "https://www.tiktok.com/@jessellum"
         },
-        "video": "https://www.youtube.com/embed/64O0NMQpuOU?si=FWQUBgKxcfSV-Ogz"
+        "video": "https://www.youtube.com/embed/KCkaZ_r9AKE?si=lLu7pdHQlvakyKUX"
     }
 
 }
